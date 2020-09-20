e3fc737c6f5bf7738befc9f73379a2f6b1328533
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-20 22:11:27 +0530

    Update Check if a string contains a specific substring.py

--- Check if a string contains a specific substring.py
+++ Check if a string contains a specific substring.py
@@ -1,4 +1,4 @@
 sen=str(input("Enter the sentence: "))
-word=str(input("Enter the word to be searched: ")) 
+word=str(input("Enter the word to be searched: "))  
 # "in" operator will return true if a string contains a  substring or false 
 print( word in sen )  
